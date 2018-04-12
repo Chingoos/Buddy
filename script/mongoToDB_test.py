6fd5f8ceb73ebefbc347b42bdff680293d2bee37
Tae Lee <leeth7830@gmail.com>
2018-04-12 01:06:30 -0700

    Update mongoToDB_test.py

--- script/mongoToDB_test.py
+++ script/mongoToDB_test.py
@@ -12,20 +12,14 @@ client = pymongo.MongoClient()
 
 db = client.staging
 
-x.execute("""DROP TABLE IF EXISTS venue_test""")
-x.execute("""CREATE TABLE venue_test (venue_id VARCHAR(50) NOT NULL, name VARCHAR(100) NOT NULL, category VARCHAR(50), verified VARCHAR(10) , address VARCHAR(100), city VARCHAR(100), state VARCHAR(20), zip_code VARCHAR(10), tipCount INT, usersCount INT, checkinsCount INT, latitude FLOAT(30,20), longitude FLOAT(30,20), as_of_date DATETIME) CHARACTER SET = utf8""")
+x.execute("""DROP TABLE IF EXISTS places_test""")
+x.execute("""CREATE TABLE places_test (place_id VARCHAR(50) NOT NULL, name VARCHAR(100) NOT NULL, address VARCHAR(100), city VARCHAR(100), state VARCHAR(20), zip_code VARCHAR(10), country VARCHAR(20), district VARCHAR(20), phone_number VARCHAR (20), yelp_id VARCHAR(50), latitude FLOAT(30,20), longitude FLOAT(30,20), as_of_date DATETIME) CHARACTER SET = utf8""")
   
-for v in db.venues.find():
-  if (v['location'].get('address','NULL') != 'NULL') and (v['location'].get('city','NULL') != 'NULL') and (v['location'].get('city','NULL') != 'NULL') and (v['location'].get('state','NULL') != 'NULL')  and (v['location'].get('postalCode','NULL') != 'NULL'):
-    x.execute("""INSERT INTO venue_test(venue_id, name, category, verified, address, city, state, zip_code, tipCount, usersCount, checkinsCount, latitude, longitude, as_of_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())""", (v['id'], v['name'].encode("utf-8"), v['categories'][0]['name'].encode("utf-8"), v['verified'],  v['location']['address'].encode("utf-8"), v['location']['city'].encode("utf-8"), v['location']['state'].encode("utf-8"), v['location']['postalCode'], v['stats']['tipCount'], v['stats']['usersCount'], v['stats']['checkinsCount'], v['location']['lat'], v['location']['lng']))
-  elif (v['location'].get('city','NULL') != 'NULL') and (v['location'].get('state','NULL') != 'NULL') and (v['stats'].get('usersCount','NULL') != 'NULL') and (v['location'].get('postalCode','NULL') != 'NULL'):
-    x.execute("""INSERT INTO venue_test(venue_id, name, category, verified, city, state, zip_code, tipCount, usersCount, checkinsCount,latitude, longitude, as_of_date) VALUES (%s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())""", (v['id'], v['name'].encode("utf-8"), v['categories'][0]['name'].encode("utf-8"), v['verified'],  v['location']['city'].encode("utf-8"), v['location']['state'].encode("utf-8"), v['location']['postalCode'], v['stats']['tipCount'], v['stats']['usersCount'], v['stats']['checkinsCount'], v['location']['lat'], v['location']['lng']))
-  elif (v['location'].get('city','NULL') != 'NULL') and (v['location'].get('state','NULL') != 'NULL'):
-    x.execute("""INSERT INTO venue_test(venue_id, name, category, verified, city, state, tipCount, usersCount, checkinsCount,latitude, longitude, as_of_date) VALUES (%s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, NOW())""", (v['id'], v['name'].encode("utf-8"), v['categories'][0]['name'].encode("utf-8"), v['verified'],  v['location']['city'].encode("utf-8"), v['location']['state'].encode("utf-8"), v['stats']['tipCount'], v['stats']['usersCount'], v['stats']['checkinsCount'], v['location']['lat'], v['location']['lng']))
-  elif (v['location'].get('state','NULL') != 'NULL'):
-    x.execute("""INSERT INTO venue_test(venue_id, name, category, verified, state, tipCount, usersCount, checkinsCount, latitude, longitude, as_of_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())""", (v['id'], v['name'].encode("utf-8"), v['categories'][0]['name'].encode("utf-8"), v['verified'], v['location']['state'].encode("utf-8"), v['stats']['tipCount'], v['stats']['usersCount'], v['stats']['checkinsCount'], v['location']['lat'], v['location']['lng']))
-  else:
-    print v['name']
+for v in db.places.find():
+  try:
+    x.execute("""INSERT INTO places_test(place_id, name, latitude, longitude, as_of_date) VALUES (%s, %s, %s, %s, NOW())""", (v['id'], v['title'], v['position'](0), v['position'](1)))
+  except:
+    print v['title']
   
 conn.commit()
 conn.close()
