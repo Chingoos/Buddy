870ebfec7eddc0680c6a2e9b123ebd65da7368b1
Tae Lee <leeth7830@gmail.com>
2018-04-12 01:58:24 -0700

    Update mongoToDB_test.py

--- script/mongoToDB_test.py
+++ script/mongoToDB_test.py
@@ -16,7 +16,7 @@ x.execute("""DROP TABLE IF EXISTS places_test""")
 x.execute("""CREATE TABLE places_test (place_id VARCHAR(50) NOT NULL, name VARCHAR(100) NOT NULL, address VARCHAR(100), city VARCHAR(100), state VARCHAR(20), zip_code VARCHAR(10), country VARCHAR(20), district VARCHAR(30), phone_number VARCHAR (20), yelp_id VARCHAR(50), latitude FLOAT(30,20), longitude FLOAT(30,20), as_of_date DATETIME) CHARACTER SET = utf8""")
   
 for v in db.places_detail.find():
-  x.execute("""INSERT INTO places_test(place_id, name, latitude, longitude, as_of_date) VALUES (%s,%s, %s, %s NOW())""", (v['placeId'], v['name'], v['location']['position'][0], v['location']['position'][1]))
+  x.execute("""INSERT INTO places_test(place_id, name, latitude, longitude, as_of_date) VALUES (%s,%s, %s, %s, NOW())""", (v['placeId'], v['name'], v['location']['position'][0], v['location']['position'][1]))
   if (v['location'].get('address', 'Null') != 'Null'):
     if (v['location']['address'].get('house','Null') != 'Null') and (v['location']['address'].get('street','Null') != 'Null'):
       x.execute("""UPDATE places_test SET address = %s WHERE place_id = %s)""", (v['location']['address']['house'] + v['location']['address']['street'], v['placeId']))
