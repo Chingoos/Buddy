d8959780034f8d5a7689dc5997bedaeda3f1cce9
Tae Lee <leeth7830@gmail.com>
2018-04-12 01:57:11 -0700

    Update mongoToDB_test.py

--- script/mongoToDB_test.py
+++ script/mongoToDB_test.py
@@ -16,15 +16,16 @@ x.execute("""DROP TABLE IF EXISTS places_test""")
 x.execute("""CREATE TABLE places_test (place_id VARCHAR(50) NOT NULL, name VARCHAR(100) NOT NULL, address VARCHAR(100), city VARCHAR(100), state VARCHAR(20), zip_code VARCHAR(10), country VARCHAR(20), district VARCHAR(30), phone_number VARCHAR (20), yelp_id VARCHAR(50), latitude FLOAT(30,20), longitude FLOAT(30,20), as_of_date DATETIME) CHARACTER SET = utf8""")
   
 for v in db.places_detail.find():
-  if (v['location'].get('address', 'Null') != 'Null') and  (v['location']['address'].get('district','Null') != 'Null'):
-    if v['location']['address'].get('house','Null') != 'Null':
-      if v['location']['address'].get('street','Null') != 'Null':
+  x.execute("""INSERT INTO places_test(place_id, name, latitude, longitude, as_of_date) VALUES (%s,%s, %s, %s NOW())""", (v['placeId'], v['name'], v['location']['position'][0], v['location']['position'][1]))
+  if (v['location'].get('address', 'Null') != 'Null'):
+    if (v['location']['address'].get('house','Null') != 'Null') and (v['location']['address'].get('street','Null') != 'Null'):
+      x.execute("""UPDATE places_test SET address = %s WHERE place_id = %s)""", (v['location']['address']['house'] + v['location']['address']['street'], v['placeId']))
+      if v['location']['address'].get('postalCode','Null') != 'Null':
+         x.execute("""UPDATE places_test SET zip_code = %s WHERE place_id = %s)""", (v['location']['address']['postalCode'], v['placeId']))
         if v['location']['address'].get('city','Null') != 'Null':
+          x.execute("""UPDATE places_test SET city = %s WHERE place_id = %s)""", (v['location']['address']['city'], v['placeId']))
           if v['location']['address'].get('district','Null') != 'Null':
-            try:
-              x.execute("""INSERT INTO places_test(place_id, name, address, city, state, zip_code, country, district,  latitude, longitude, as_of_date) VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())""", (v['placeId'], v['name'], v['location']['address']['house'] + v['location']['address']['street'], v['location']['address']['city'],  v['location']['address']['state'],  v['location']['address']['postalCode'],  v['location']['address']['country'],  v['location']['address']['district'], v['location']['position'][0], v['location']['position'][1]))
-            except:
-              print v['name']
+            x.execute("""UPDATE places_test SET state = %s WHERE place_id = %s)""", (v['location']['address']['state'], v['placeId']))
           else:
             print("NO DISTRICT" + v['name'])
         else:
@@ -32,9 +33,9 @@ for v in db.places_detail.find():
       else: 
         print("NO STREET" + v['name'])
     else:
-      x.execute("""INSERT INTO places_test(place_id, name, city, state, zip_code, country, district,  latitude, longitude, as_of_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())""", (v['placeId'], v['name'], v['location']['address']['city'],  v['location']['address']['state'],  v['location']['address']['postalCode'],  v['location']['address']['country'],  v['location']['address']['district'], v['location']['position'][0], v['location']['position'][1]))
+      print("NO STREET" + v['name'])
   else:
-    x.execute("""INSERT INTO places_test(place_id, name, city, state, zip_code, country,  latitude, longitude, as_of_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())""", (v['placeId'], v['name'], v['location']['address']['city'],  v['location']['address']['state'],  v['location']['address']['postalCode'],  v['location']['address']['country'],  v['location']['position'][0], v['location']['position'][1]))
+    print("NO STREET" + v['name'])
 
   
 conn.commit()
