72357c53b16345556c3651763e796c4af82ed8e7
Tae Lee <leeth7830@gmail.com>
2018-04-12 01:30:51 -0700

    Update mongoToDB_test.py

--- script/mongoToDB_test.py
+++ script/mongoToDB_test.py
@@ -17,7 +17,7 @@ x.execute("""CREATE TABLE places_test (place_id VARCHAR(50) NOT NULL, name VARCH
   
 for v in db.places_detail.find():
   try:
-    x.execute("""INSERT INTO places_test(place_id, name, address, city, state, zip_code, country, district,  latitude, longitude, as_of_date) VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())""", (v['placeId'], v['name'], (v['location']['address']['house'] + v['location']['address']['street'], v['location']['address']['city'],  v['location']['address']['state'],  v['location']['address']['postalCode'],  v['location']['address']['country'],  v['location']['address']['district'], v['location']['position'][0], v['location']['position'][1]))
+    x.execute("""INSERT INTO places_test(place_id, name, address, city, state, zip_code, country, district,  latitude, longitude, as_of_date) VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())""", (v['placeId'], v['name'], v['location']['address']['house'] + v['location']['address']['street'], v['location']['address']['city'],  v['location']['address']['state'],  v['location']['address']['postalCode'],  v['location']['address']['country'],  v['location']['address']['district'], v['location']['position'][0], v['location']['position'][1]))
   except:
     print v['name']
 
