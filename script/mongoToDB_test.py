18bbe434143a87abc21b6cbe25229181c97e4493
Tae Lee <leeth7830@gmail.com>
2018-04-12 01:48:07 -0700

    Update mongoToDB_test.py

--- script/mongoToDB_test.py
+++ script/mongoToDB_test.py
@@ -16,7 +16,7 @@ x.execute("""DROP TABLE IF EXISTS places_test""")
 x.execute("""CREATE TABLE places_test (place_id VARCHAR(50) NOT NULL, name VARCHAR(100) NOT NULL, address VARCHAR(100), city VARCHAR(100), state VARCHAR(20), zip_code VARCHAR(10), country VARCHAR(20), district VARCHAR(30), phone_number VARCHAR (20), yelp_id VARCHAR(50), latitude FLOAT(30,20), longitude FLOAT(30,20), as_of_date DATETIME) CHARACTER SET = utf8""")
   
 for v in db.places_detail.find():
-  if v['location'].get('address', 'Null') != 'Null':
+  if (v['location'].get('address', 'Null') != 'Null') and  (v['location']['address'].get('district','Null') != 'Null'):
     if v['location']['address'].get('house','Null') != 'Null':
       if v['location']['address'].get('street','Null') != 'Null':
         if v['location']['address'].get('city','Null') != 'Null':
@@ -34,7 +34,7 @@ for v in db.places_detail.find():
     else:
       x.execute("""INSERT INTO places_test(place_id, name, city, state, zip_code, country, district,  latitude, longitude, as_of_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())""", (v['placeId'], v['name'], v['location']['address']['city'],  v['location']['address']['state'],  v['location']['address']['postalCode'],  v['location']['address']['country'],  v['location']['address']['district'], v['location']['position'][0], v['location']['position'][1]))
   else:
-    print("NO ADDRESS" + v['name'])
+    x.execute("""INSERT INTO places_test(place_id, name, city, state, zip_code, country,  latitude, longitude, as_of_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())""", (v['placeId'], v['name'], v['location']['address']['city'],  v['location']['address']['state'],  v['location']['address']['postalCode'],  v['location']['address']['country'],  v['location']['position'][0], v['location']['position'][1]))
 
   
 conn.commit()
