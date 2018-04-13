aecd1ba968697044b25b0549bb280a76c40a0009
Tae Lee <leeth7830@gmail.com>
2018-04-12 18:16:00 -0700

    Update mongoToDB_test.py

--- script/mongoToDB_test.py
+++ script/mongoToDB_test.py
@@ -14,7 +14,10 @@ db = client.staging
 
 x.execute("""DROP TABLE IF EXISTS places_test""")
 x.execute("""CREATE TABLE places_test (place_id VARCHAR(50) NOT NULL, name VARCHAR(100) NOT NULL, address VARCHAR(100), city VARCHAR(100), state VARCHAR(20), zip_code VARCHAR(10), country VARCHAR(20), district VARCHAR(30), phone_number VARCHAR (20), yelp_id VARCHAR(50), latitude FLOAT(30,20), longitude FLOAT(30,20), as_of_date DATETIME) CHARACTER SET = utf8""")
-  
+x.execute("""DROP TABLE IF EXISTS places_category""")
+x.execute("""CREATE TABLE places_category (place_id VARCHAR(50) NOT NULL, category VARCHAR(50) NOT NULL, category_id VARCHAR(50) NOT NULL)""")
+x.execute("""DROP TABLE IF EXISTS places_image""")
+x.execute("""CREATE TABLE places_images (place_id VARCHAR(50) NOT NULL, image_src VARCHAR(200), image_id VARCHAR(50))""")
 for v in db.places_detail.find():
   x.execute("""INSERT INTO places_test(place_id, name, latitude, longitude, as_of_date) VALUES (%s,%s, %s, %s, NOW())""", (v['placeId'], v['name'].encode('ascii', 'ignore').decode('ascii'), v['location']['position'][0], v['location']['position'][1]))
   if (v['location'].get('address', 'Null') != 'Null'):
@@ -30,13 +33,24 @@ for v in db.places_detail.find():
       x.execute("""UPDATE places_test SET district = %s WHERE place_id = %s""", (v['location']['address']['district'], v['placeId']))
     if v['location']['address'].get('country','Null') != 'Null':
       x.execute("""UPDATE places_test SET country = %s WHERE place_id = %s""", (v['location']['address']['country'], v['placeId']))
+  else:
+    print(v['name'])
   if v.get('contacts','Null') != 'Null':
     if v['contacts'].get('phone','Null') != 'Null':
       if v['contacts']['phone'][0].get('value','Null') != 'Null':
         x.execute("""UPDATE places_test SET phone_number = %s WHERE place_id = %s""", (v['contacts']['phone'][0]['value'], v['placeId']))
   else:
     print(v['name'])
+  if v.get('categories', 'Null') != 'Null':
+    for c in v['categories']:
+      x.execute("""INSERT INTO places_category(place_id, category, category_id) VALUES (%s, %s, %s)""", (v['placeId'], c['title'], c['id']))
+  if v.get('media', 'Null') != 'Null':
+    if v['media'].get('images', 'Null') != 'Null':
+      if v['media']['images'].get('items', 'Null') != 'Null':
+        for i in v['media']['images']['items']:
+          x.execute("""INSERT INTO places_images(place_id, image_src, image_id) VALUES (%s, %s, %s) """, (v['placeId'], i['src'], i['id']))
 
+for 
   
 conn.commit()
 conn.close()
